721713bf662cd93ce01b03d80a85a83ae1a740ac
Bradley Plunkett <bradleyplunkett@Bradleys-MacBook-Pro.local>
2021-12-22 11:00:15 -0700

    added set

---
+++ Data Structures/Set/createSetClass.js
@@ -0,0 +1,38 @@
+//A set is like an array, but it cannot contain duplicate values. The typical use for a set is to simply check for the presence of an item
+
+class Set {
+    constructor() {
+      // Dictionary will hold the items of our set
+      this.dictionary = {};
+    }
+  
+    // This method will check for the presence of an element and return true or false
+    has(element) {
+      return this.dictionary[element] !== undefined;
+    }
+  
+    // This method will return all the values in the set
+    values() {
+      return Object.values(this.dictionary);
+    }
+  
+    add (element) {
+      if (!this.has(element)){
+     this.dictionary[element] = element;
+     return true
+      } else {
+      return false
+    }}
+  
+    remove(element) {
+      if (this.has(element)){
+        delete this.dictionary[element];
+        return true
+      }
+      else {return false}
+    }
+  
+    size() {
+      return Object.keys(this.dictionary).length
+    }
+  }
